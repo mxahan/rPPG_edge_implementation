1af2765c27773b33947e558670e32a6c8fac8155
mxahan <smxahsan@gmail.com>
2020-11-26 14:46:33 -0500

    new

---
+++ vid_read.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+
+def video_reader():
+    data = []
+    im_size = (100,100)
+    
+    cap = cv2.VideoCapture('im_test1.mkv')
+    
+
+    
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        
+        if ret==False:
+            break
+    
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        gray  = gray[:,:,1]
+
+        
+       
+        gray = cv2.resize(gray, im_size)
+        
+        # pdb.set_trace()
+       
+        data.append(gray)
+        
+        cv2.imshow('frame', gray)
+        
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    
+    fps = cap.get(cv2.CAP_PROP_FPS)
+        
+    cap.release()
+    cv2.destroyAllWindows()
+    data =  np.array(data)
+    
+    return fps, data
